6ec16cccf9bb6ee62963603d2bc89c6b0a072459
ilya2s <ilyass.elouazzani97@outlook.fr>
2022-07-20 22:46:34 -0400

    configured project dependencies and structure

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module ca.umontreal.iro.fg {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens ca.umontreal.iro.fg to javafx.fxml;
+    exports ca.umontreal.iro.fg;
+}
