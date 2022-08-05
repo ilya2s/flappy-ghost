d31934e0a671db8535c3f2646f614d13e52fadb0
memehtn <ng.trinh.t@gmail.com>
2022-08-04 22:04:45 -0400

    fix merge conflict

--- src/main/java/ca/umontreal/iro/fg/obstacles/QuanticObstacle.java
+++ src/main/java/ca/umontreal/iro/fg/obstacles/QuanticObstacle.java
@@ -7,6 +7,9 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.util.Duration;
 
+/**
+ * T
+ */
 public class QuanticObstacle extends Obstacle {
 
     private static final double PERIOD = 0.2;
@@ -37,35 +40,6 @@ public class QuanticObstacle extends Obstacle {
     public void update(double dt) {
         double nextX = x - dt * Ghost.INIT_SPEED;
 
-<<<<<<< HEAD
-        double rangeX = (Math.random() * 60);
-        if ( rangeX < 30 ) {
-            newX = rangeX;
-            //imageView.setImage(null);
-            //setX( (nextX - rangeX) );
-            //System.out.println( " UNDER range 30    " + (nextX - rangeX));
-            //imageView.setImage(image);
-        } else if (rangeX >= 30) {
-            //setX( nextX + rangeX );
-            newX = rangeX;
-            //System.out.println( "OVER range 30    " + nextX + rangeX);
-        }
-        //imageView.setImage(image);
-
-        double rangeY = (Math.random() * 60);
-        if ( rangeY < 30 ) {
-            nextY = rangeY;
-            //imageView.setImage(null);
-            setY( (oldY - rangeY) );
-            //imageView.setImage(image);
-        } else if (rangeY >= 30) {
-            nextY = rangeY;
-            setY( oldY + rangeY );
-        }
-
-
-=======
->>>>>>> c2b17268b4742c5e61d9e6de03424d7d2459548a
         if (nextX + radius + RANGE < 0) {
             out = true;
         }
